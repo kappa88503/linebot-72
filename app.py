9f82219f2109c7da1e24b868959dce079fe79086
StylisticEwe72 <77837611+StylisticEwe72@users.noreply.github.com>
2021-09-14 21:47:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,21 @@ from linebot.models import (
 )
 import random
 from datetime import datetime
-
+# -----------------------------------------------------------------------------------------------
 app = Flask(__name__)
-
+# -----------------------------------------------------------------------------------------------
 line_bot_api = LineBotApi('vpVg86tVKnbsIFw2JlicqbxPhzIeD246B5asVzbwMBCaalUGr5rdb1ghL5SeJeD+mJAWgqzH+QGDL76KA6Ns7uyPjK57WhartmXtxcZFxeOaB14PyEp6ITfKRguKnBSQswi+5kQixxzWvx0VLNdk4QdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('ed9c515dac10991c798d4e2a559fa667')
+# -----------------------------------------------------------------------------------------------
+# def
 
-
+# -----------------------------------------------------------------------------------------------
+wek_curriculum = [['跑班選修', '跑班選修', '跑班選修', '跑班選修', '選物四', '數學甲', '選化三'],
+                  ['英語文', '國語文', '國語文', '數學甲', '英文作文', '藝生', '藝生', '化學課輔'],
+                  ['數學甲', '彈性學習', '彈性學習', '彈性學習', '選物三-1', '體育', '國語文'],
+                  ['數學甲', '國語文', '跑班選修', '跑班選修', '英語文', '選物四', '選化五-1'],
+                  ['英文作文', '體育', '綜合活動', '綜合活動', '選化三', '跑班選修', '跑班選修']]
+# -----------------------------------------------------------------------------------------------
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -35,7 +43,7 @@ def callback():
         abort(400)
 
     return 'OK'
-
+# -----------------------------------------------------------------------------------------------
 
 
 @handler.add(MessageEvent, message=TextMessage)
@@ -105,8 +113,32 @@ def handle_message(event):
         
     if line_text == "test" or line_text == "Test":
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='伺服器連線正常'))
-    
 
-        
+    if ('星期' in line_text or '禮拜' in line_text) and '課表' in line_text:
+        txt = list(line_text)
+        if txt[2] == '一':
+            print('\n'.join(wek_curriculum[0]))
+
+        elif txt[2] == '二':
+            print('\n'.join(wek_curriculum[1]))
+
+        elif txt[2] == '三':
+            print('\n'.join(wek_curriculum[2]))
+
+        elif txt[2] == '四':
+            print('\n'.join(wek_curriculum[3]))
+
+        elif txt[2] == '五':
+            print('\n'.join(wek_curriculum[4]))
+
+        elif txt[2] == '六' or txt[2] == '日':
+            print('放假啦')
+
+        else:
+            print(f'沒有星期{txt[2:-2]}')
+
+
+
+ # -----------------------------------------------------------------------------------------------       
 if __name__ == "__main__":
     app.run()
