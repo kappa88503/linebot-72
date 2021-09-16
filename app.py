9c743f4c9d2cb097cc8e0f99465cc41f3ecaaebd
StylisticEwe72 <77837611+StylisticEwe72@users.noreply.github.com>
2021-09-16 20:30:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # app.py
-from flask import Flask, request, abort
+import random
+import time
 
+from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -10,12 +12,12 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-import random
-import time
+
 # -----------------------------------------------------------------------------------------------
 app = Flask(__name__)
 # -----------------------------------------------------------------------------------------------
-line_bot_api = LineBotApi('vpVg86tVKnbsIFw2JlicqbxPhzIeD246B5asVzbwMBCaalUGr5rdb1ghL5SeJeD+mJAWgqzH+QGDL76KA6Ns7uyPjK57WhartmXtxcZFxeOaB14PyEp6ITfKRguKnBSQswi+5kQixxzWvx0VLNdk4QdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi(
+    'vpVg86tVKnbsIFw2JlicqbxPhzIeD246B5asVzbwMBCaalUGr5rdb1ghL5SeJeD+mJAWgqzH+QGDL76KA6Ns7uyPjK57WhartmXtxcZFxeOaB14PyEp6ITfKRguKnBSQswi+5kQixxzWvx0VLNdk4QdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('ed9c515dac10991c798d4e2a559fa667')
 # -----------------------------------------------------------------------------------------------
 # def
@@ -26,6 +28,8 @@ wek_curriculum = [['跑班選修', '跑班選修', '跑班選修', '跑班選修
                   ['數學甲', '彈性學習', '彈性學習', '彈性學習', '午休', '選物三-1', '體育', '國語文', '放學啦'],
                   ['數學甲', '國語文', '跑班選修', '跑班選修', '午休', '英語文', '選物四', '選化五-1', '放學啦'],
                   ['英文作文', '體育', '綜合活動', '綜合活動', '午休', '選化三', '跑班選修', '跑班選修', '放學啦']]
+
+
 # -----------------------------------------------------------------------------------------------
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -43,6 +47,8 @@ def callback():
         abort(400)
 
     return 'OK'
+
+
 # -----------------------------------------------------------------------------------------------
 
 
@@ -50,23 +56,23 @@ def callback():
 def handle_message(event):
     global wek_curriculum
     msg = event.message.text
-    #print(msg)
+    # print(msg)
     msg = msg.encode('utf-8')
     line_text = event.message.text
-        
+
     if line_text == "骰子":
-        r = random.randint(1,6)
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=f'擲出 {r} 點'))
-        
+        r = random.randint(1, 6)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f'擲出 {r} 點'))
+
     if line_text == "猜拳":
         mora_list = ['剪刀', '石頭', '布']
         mora = str(random.choice(mora_list))
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=mora))        
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=mora))
 
     if line_text == "不要問給我來一本正經的漫畫":
         sex_book = 'https://nhentai.net/g/' + str(random.randint(111111, 369999))
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=sex_book))
-        
+
     if '我出' in line_text:
         mora_list = ['剪刀', '石頭', '布']
         if '剪刀' in line_text:
@@ -102,18 +108,18 @@ def handle_message(event):
                 mora_end = '沒輸沒贏'
             mora_txt = f'你出:{mora_player}\n我出:{a}\n\n結果:{mora_end}'
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=mora_txt))
-     
+
     if '不要問選一個 ' in line_text:
         event.message.text = event.message.text.replace('不要問選一個 ', '')
         ran_ran = event.message.text.split('/')
         ran_ran = f'我選 {random.choice(ran_ran)}'
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=ran_ran))
-        
+
     if line_text.lower() == "test":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='伺服器連線正常'))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='伺服器連線正常'))
 
     if ('星期' in line_text or '禮拜' in line_text) and '課表' in line_text:
-        
+
         txt = list(line_text)
         if txt[2] == '一':
             cur_text = '\n'.join(wek_curriculum[0])
@@ -130,16 +136,44 @@ def handle_message(event):
         elif txt[2] == '五':
             cur_text = '\n'.join(wek_curriculum[4])
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=cur_text))
-        elif txt[2] == '六' or  txt[2] == '日':
+        elif txt[2] == '六' or txt[2] == '日':
             cur_text = '放假啦'
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=cur_text))
         else:
             cur_text = f'沒有星期{txt[2:-2]}'
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=cur_text))
+    if line_text == '下一節':
+        t = time.localtime()
+        wek = t.tm_wday + 1
+        th = t.tm_hour
+        tm = t.tm_min
+        if wek <= 5:
+            if 6 <= th <= 17:
+                if th == 6 or th == 7 or (th == 8 and tm <= 10):
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=wek_curriculum[wek - 1][0]))
+                elif th == 8 and tm >= 10:
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=wek_curriculum[wek - 1][1]))
+                elif th == 9:
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=wek_curriculum[wek - 1][2]))
+                elif th == 10:
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=wek_curriculum[wek - 1][3]))
+                elif th == 11:
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=wek_curriculum[wek - 1][4]))
+                elif th == 12:
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=wek_curriculum[wek - 1][5]))
+                elif th == 13 or (th == 14 and tm <= 10):
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=wek_curriculum[wek - 1][6]))
+                elif th == 14:
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=wek_curriculum[wek - 1][7]))
+                elif th == 15:
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=wek_curriculum[wek - 1][8]))
+            else:
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text='放學啦'))
+        else:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='放假啦'))
 
 
-
-
- # -----------------------------------------------------------------------------------------------       
+# -----------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     app.run()
+
