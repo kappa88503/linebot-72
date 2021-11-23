b6baaea31553c11c27475cb52429412ad4abb583
StylisticEwe72 <77837611+StylisticEwe72@users.noreply.github.com>
2021-11-23 21:18:23 +0800

    Update app.py
    debug 添加函式 from linebot.models import *

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-
+from linebot.models import *
 # -----------------------------------------------------------------------------------------------
 app = Flask(__name__)
 # -----------------------------------------------------------------------------------------------
