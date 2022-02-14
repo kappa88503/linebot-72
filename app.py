89fcab7b8da179aae3e6fadbfd78586916537470
StylisticEwe72 <u66172115a@gmail.com>
2022-02-14 11:41:00 +0800

    改變命名規則

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ def handle_message(event):
             if i != '':
                 check = True
                 break
-        if check:
+        if check == False:
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text='姓名請勿為空'))
         else:
             user_id = event.source.user_id
